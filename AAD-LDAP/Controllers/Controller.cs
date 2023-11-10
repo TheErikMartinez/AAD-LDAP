1dc766665349dcb91831603a400bb9317d7a956b
HUNGARIA\MARTINEZ <erik.adan.martinez@koerber.com>
2023-11-10 13:48:50 +0100

    initial commit

---
+++ AAD-LDAP/Controllers/Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Claims;
+using System.Text;
+
+namespace AAD_LDAP.Controllers
+{
+    
+}
+    
+
---
+++ AAD-LDAP/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AAD_LDAP.Models
+{
+    public class User
+    {
+        public string name {  get; set; }
+        public string sAMAccountName { get; set; }
+        public string department { get; set; }
+        [EmailAddress]
+        public string mail { get; set; }
+        public string extensionAttribute { get; set; }
+        public string manager { get; set; }
+    }
+}
